1af4ce4d58e075425094da331e284a57d52b4416
Gustavo A. R. Silva <garsilva@embeddedor.com>
2017-05-04 20:38:20 -0500

    RDMA/core: Document confusing code
    [ Upstream commit 28b5b3a23ba67970f4f534b15c4e4d687136605a ]
    
    While looking into Coverity ID 1351047 I ran into the following
    piece of code at
    drivers/infiniband/core/verbs.c:496:
    
    ret = rdma_addr_find_l2_eth_by_grh(&dgid, &sgid,
                                       ah_attr->dmac,
                                       wc->wc_flags & IB_WC_WITH_VLAN ?
                                       NULL : &vlan_id,
                                       &if_index, &hoplimit);
    
    The issue here is that the position of arguments in the call to
    rdma_addr_find_l2_eth_by_grh() function do not match the order of
    the parameters:
    
    &dgid is passed to sgid
    &sgid is passed to dgid
    
    This is the function prototype:
    
    int rdma_addr_find_l2_eth_by_grh(const union ib_gid *sgid,
     				 const union ib_gid *dgid,
     				 u8 *dmac, u16 *vlan_id, int *if_index,
     				 int *hoplimit)
    
    My question here is if this is intentional?
    
    Answer:
    Yes. ib_init_ah_from_wc() creates ah from the incoming packet.
    Incoming packet has dgid of the receiver node on which this code is
    getting executed and sgid contains the GID of the sender.
    
    When resolving mac address of destination, you use arrived dgid as
    sgid and use sgid as dgid because sgid contains destinations GID whom to
    respond to.
    
    Signed-off-by: Gustavo A. R. Silva <garsilva@embeddedor.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- drivers/infiniband/core/verbs.c
+++ drivers/infiniband/core/verbs.c
@@ -448,6 +448,19 @@ static int get_gids_from_rdma_hdr(union rdma_network_hdr *hdr,
 	}
 }
 
+/*
+ * This function creates ah from the incoming packet.
+ * Incoming packet has dgid of the receiver node on which this code is
+ * getting executed and, sgid contains the GID of the sender.
+ *
+ * When resolving mac address of destination, the arrived dgid is used
+ * as sgid and, sgid is used as dgid because sgid contains destinations
+ * GID whom to respond to.
+ *
+ * This is why when calling rdma_addr_find_l2_eth_by_grh() function, the
+ * position of arguments dgid and sgid do not match the order of the
+ * parameters.
+ */
 int ib_init_ah_from_wc(struct ib_device *device, u8 port_num,
 		       const struct ib_wc *wc, const struct ib_grh *grh,
 		       struct ib_ah_attr *ah_attr)
