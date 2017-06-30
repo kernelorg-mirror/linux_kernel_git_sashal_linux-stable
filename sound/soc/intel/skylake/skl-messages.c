ffd74a0c06fef7eaa7492811c07e674c89a16601
Subhransu S. Prusty <subhransu.s.prusty@intel.com>
2017-06-30 18:38:44 +0530

    ASoC: Intel: Skylake: Fix default dma_buffer_size
    [ Upstream commit 5b43af6d25461d7de293e0704d3b4631dda9b1e8 ]
    
    If the dma_buffer_size is not defined in topology, fix it to 2ms default
    value to make backward compatible.
    
    Fixes: f6e6ab1d16ec ("ASoC: Intel: Skylake: Fix dma buffer size calculation")
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Acked-By: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- sound/soc/intel/skylake/skl-messages.c
+++ sound/soc/intel/skylake/skl-messages.c
@@ -489,6 +489,14 @@ static void skl_setup_cpr_gateway_cfg(struct skl_sst *ctx,
 	else
 		cpr_mconfig->gtw_cfg.dma_buffer_size = 2 * mconfig->ibs;
 
+	/* fallback to 2ms default value */
+	if (!cpr_mconfig->gtw_cfg.dma_buffer_size) {
+		if (mconfig->hw_conn_type == SKL_CONN_SOURCE)
+			cpr_mconfig->gtw_cfg.dma_buffer_size = 2 * mconfig->obs;
+		else
+			cpr_mconfig->gtw_cfg.dma_buffer_size = 2 * mconfig->ibs;
+	}
+
 	cpr_mconfig->cpr_feature_mask = 0;
 	cpr_mconfig->gtw_cfg.config_length  = 0;
 
