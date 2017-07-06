6ca0404cd6521d744dabb9e0441da9d21d4b0abc
Christian Borntraeger <borntraeger@de.ibm.com>
2017-07-06 14:44:28 +0200

    KVM: mark vcpu->pid pointer as rcu protected
    [ Upstream commit 0e4524a5d341e719e8ee9ee7db5d58e2c5a4c10e ]
    
    We do use rcu to protect the pid pointer. Mark it as such and
    adopt all code to use the proper access methods.
    
    This was detected by sparse.
    "virt/kvm/kvm_main.c:2248:15: error: incompatible types in comparison
    expression (different address spaces)"
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -226,7 +226,7 @@ struct kvm_vcpu {
 	int guest_fpu_loaded, guest_xcr0_loaded;
 	unsigned char fpu_counter;
 	struct swait_queue_head wq;
-	struct pid *pid;
+	struct pid __rcu *pid;
 	int sigset_active;
 	sigset_t sigset;
 	struct kvm_vcpu_stat stat;
