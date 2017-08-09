bdda0f04b4bc7720d84c7b80e3b528ce1988d7db
Liu Bo <bo.li.liu@oracle.com>
2017-08-09 11:10:16 -0600

    Btrfs: fix out of bounds array access while reading extent buffer
    [ Upstream commit f716abd55d1e141772fd44a99c07c89f4aff1978 ]
    
    There is a corner case that slips through the checkers in functions
    reading extent buffer, ie.
    
    if (start < eb->len) and (start + len > eb->len),
    then
    
    a) map_private_extent_buffer() returns immediately because
    it's thinking the range spans across two pages,
    
    b) and the checkers in read_extent_buffer(), WARN_ON(start > eb->len)
    and WARN_ON(start + len > eb->start + eb->len), both are OK in this
    corner case, but it'd actually try to access the eb->pages out of
    bounds because of (start + len > eb->len).
    
    The case is found by switching extent inline ref type from shared data
    ref to non-shared data ref, which is a kind of metadata corruption.
    
    It'd use the wrong helper to access the eb,
    eg. btrfs_extent_data_ref_root(eb, ref) is used but the %ref passing
    here is "struct btrfs_shared_data_ref".  And if the extent item
    happens to be the first item in the eb, then offset/length will get
    over eb->len which ends up an invalid memory access.
    
    This is adding proper checks in order to avoid invalid memory access,
    ie. 'general protection fault', before it's too late.
    
    Reviewed-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
    Signed-off-by: Chris Mason <clm@fb.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -5331,8 +5331,12 @@ void read_extent_buffer(struct extent_buffer *eb, void *dstv,
 	size_t start_offset = eb->start & ((u64)PAGE_SIZE - 1);
 	unsigned long i = (start_offset + start) >> PAGE_SHIFT;
 
-	WARN_ON(start > eb->len);
-	WARN_ON(start + len > eb->start + eb->len);
+	if (start + len > eb->len) {
+		WARN(1, KERN_ERR "btrfs bad mapping eb start %llu len %lu, wanted %lu %lu\n",
+		     eb->start, eb->len, start, len);
+		memset(dst, 0, len);
+		return;
+	}
 
 	offset = (start_offset + start) & (PAGE_SIZE - 1);
 
@@ -5405,6 +5409,12 @@ int map_private_extent_buffer(struct extent_buffer *eb, unsigned long start,
 	unsigned long end_i = (start_offset + start + min_len - 1) >>
 		PAGE_SHIFT;
 
+	if (start + min_len > eb->len) {
+		WARN(1, KERN_ERR "btrfs bad mapping eb start %llu len %lu, wanted %lu %lu\n",
+		       eb->start, eb->len, start, min_len);
+		return -EINVAL;
+	}
+
 	if (i != end_i)
 		return 1;
 
@@ -5414,12 +5424,6 @@ int map_private_extent_buffer(struct extent_buffer *eb, unsigned long start,
 	} else {
 		offset = 0;
 		*map_start = ((u64)i << PAGE_SHIFT) - start_offset;
-	}
-
-	if (start + min_len > eb->len) {
-		WARN(1, KERN_ERR "btrfs bad mapping eb start %llu len %lu, wanted %lu %lu\n",
-		       eb->start, eb->len, start, min_len);
-		return -EINVAL;
 	}
 
 	p = eb->pages[i];
