d57e4c8f3050828fcec46c5111f2ead8b20c1f01
Dan Carpenter <dan.carpenter@oracle.com>
2017-09-30 11:15:29 +0300

    usb: misc: usbtest: Fix overflow in usbtest_do_ioctl()
    [ Upstream commit cb84f56861eb333af0a5bab475d741b13067c05c ]
    
    There used to be a test against "if (param->sglen > MAX_SGLEN)" but it
    was removed during a refactor.  It leads to an integer overflow and a
    stack overflow in test_queue() if we try to create a too large urbs[]
    array on the stack.
    
    There is a second integer overflow in test_queue() as well if
    "param->iterations" is too high.  I don't immediately see that it's
    harmful but I've added a check to prevent it and silence the static
    checker warning.
    
    Fixes: 18fc4ebdc705 ("usb: misc: usbtest: Remove timeval usage")
    Acked-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- drivers/usb/misc/usbtest.c
+++ drivers/usb/misc/usbtest.c
@@ -1972,6 +1972,9 @@ test_queue(struct usbtest_dev *dev, struct usbtest_param_32 *param,
 	int			status = 0;
 	struct urb		*urbs[param->sglen];
 
+	if (!param->sglen || param->iterations > UINT_MAX / param->sglen)
+		return -EINVAL;
+
 	memset(&context, 0, sizeof(context));
 	context.count = param->iterations * param->sglen;
 	context.dev = dev;
@@ -2094,6 +2097,8 @@ usbtest_do_ioctl(struct usb_interface *intf, struct usbtest_param_32 *param)
 	int	retval = -EOPNOTSUPP;
 
 	if (param->iterations <= 0)
+		return -EINVAL;
+	if (param->sglen > MAX_SGLEN)
 		return -EINVAL;
 	/*
 	 * Just a bunch of test cases that every HCD is expected to handle.
