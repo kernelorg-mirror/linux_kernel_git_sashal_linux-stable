ae8fb802fa6ad95f8e7e66ed255afbd49d045cb6
Michael Chan <michael.chan@broadcom.com>
2017-10-13 21:09:33 -0400

    bnxt_en: Fix possible corrupted NVRAM parameters from firmware response.
    [ Upstream commit cc72f3b1feb4fd38d33ab7a013d5ab95041cb8ba ]
    
    In bnxt_find_nvram_item(), it is copying firmware response data after
    releasing the mutex.  This can cause the firmware response data
    to be corrupted if the next firmware response overwrites the response
    buffer.  The rare problem shows up when running ethtool -i repeatedly.
    
    Fix it by calling the new variant _hwrm_send_message_silent() that requires
    the caller to take the mutex and to release it after the response data has
    been copied.
    
    Fixes: 3ebf6f0a09a2 ("bnxt_en: Add installed-package version reporting via Ethtool GDRVINFO")
    Reported-by: Sarveswara Rao Mygapula <sarveswararao.mygapula@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -1219,6 +1219,7 @@ static inline void bnxt_disable_poll(struct bnxt_napi *bnapi)
 void bnxt_set_ring_params(struct bnxt *);
 void bnxt_hwrm_cmd_hdr_init(struct bnxt *, void *, u16, u16, u16);
 int _hwrm_send_message(struct bnxt *, void *, u32, int);
+int _hwrm_send_message_silent(struct bnxt *bp, void *msg, u32 len, int timeout);
 int hwrm_send_message(struct bnxt *, void *, u32, int);
 int hwrm_send_message_silent(struct bnxt *, void *, u32, int);
 int bnxt_hwrm_set_coal(struct bnxt *);
