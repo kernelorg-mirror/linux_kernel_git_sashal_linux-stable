142f595746428a4011429c7b2825edc6cde3f0b6
Michal Wajdeczko <michal.wajdeczko@intel.com>
2018-06-18 11:18:20 +0000

    drm/i915/guc: Print CTL params passed to Guc
    [ Upstream commit 1ea29bbd47cf5640fd67f8bcca1f934aa468c428 ]
    
    While debugging we may want to examine params passed to GuC.
    
    v2: drop #ifdef DEBUG_GUC - Michal
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Michel Thierry <michel.thierry@intel.com>
    Reviewed-by: Michel Thierry <michel.thierry@intel.com> #1
    Cc: Michal Winiarski <michal.winiarski@intel.com>
    Reviewed-by: Michał Winiarski <michal.winiarski@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180618111821.47088-1-michal.wajdeczko@intel.com
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- drivers/gpu/drm/i915/intel_guc.c
+++ drivers/gpu/drm/i915/intel_guc.c
@@ -272,6 +272,9 @@ void intel_guc_init_params(struct intel_guc *guc)
 		params[GUC_CTL_FEATURE] &= ~GUC_CTL_DISABLE_SCHEDULER;
 	}
 
+	for (i = 0; i < GUC_CTL_MAX_DWORDS; i++)
+		DRM_DEBUG_DRIVER("param[%2d] = %#x\n", i, params[i]);
+
 	/*
 	 * All SOFT_SCRATCH registers are in FORCEWAKE_BLITTER domain and
 	 * they are power context saved so it's ok to release forcewake
