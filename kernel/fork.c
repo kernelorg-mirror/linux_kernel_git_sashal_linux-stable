9ebe7c0450e706049ef6bd09aad103c4f713b281
Eric W. Biederman <ebiederm@xmission.com>
2018-07-13 15:30:33 -0500

    fork: Move and describe why the code examines PIDNS_ADDING
    [ Upstream commit 4ca1d3ee46130e9b939c02a93e3970dad151fed6 ]
    
    Normally this would be something that would be handled by handling
    signals that are sent to a group of processes but in this case the
    forking process is not a member of the group being signaled.  Thus
    special code is needed to prevent a race with pid namespaces exiting,
    and fork adding new processes within them.
    
    Move this test up before the signal restart just in case signals are
    also pending.  Fatal conditions should take presedence over restarts.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1935,6 +1935,12 @@ static __latent_entropy struct task_struct *copy_process(
 
 	rseq_fork(p, clone_flags);
 
+	/* Don't start children in a dying pid namespace */
+	if (unlikely(!(ns_of_pid(pid)->pid_allocated & PIDNS_ADDING))) {
+		retval = -ENOMEM;
+		goto bad_fork_cancel_cgroup;
+	}
+
 	/*
 	 * Process group and session signals need to be delivered to just the
 	 * parent before the fork or both the parent and the child after the
@@ -1948,10 +1954,7 @@ static __latent_entropy struct task_struct *copy_process(
 		retval = -ERESTARTNOINTR;
 		goto bad_fork_cancel_cgroup;
 	}
-	if (unlikely(!(ns_of_pid(pid)->pid_allocated & PIDNS_ADDING))) {
-		retval = -ENOMEM;
-		goto bad_fork_cancel_cgroup;
-	}
+
 
 	if (likely(p->pid)) {
 		ptrace_init_task(p, (clone_flags & CLONE_PTRACE) || trace);
