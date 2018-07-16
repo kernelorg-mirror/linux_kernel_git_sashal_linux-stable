17d883dfd3f4e7ceca23cf72d49e18d0f7048750
Felix Kuehling <Felix.Kuehling@amd.com>
2018-07-16 19:10:37 -0400

    drm/amdkfd: Call kfd2kgd.set_compute_idle
    [ Upstream commit b5aa3f4aef724e9c0f626dcf69948b22efcc5176 ]
    
    User mode queue submissions don't go through KFD. Therefore we don't
    know exactly when compute is idle or not idle. We use the existence
    of user mode queues on a device as an approximation.
    
    register_process is called when the first queue of a process is
    created. Conversely unregister_process is called when the last queue
    is destroyed. The first process that is registered takes compute
    out of idle. The last process that is unregisters sets compute back
    to idle.
    
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Reviewed-by: Eric Huang <JinHuiEric.Huang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@gmail.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
@@ -769,7 +769,8 @@ static int register_process(struct device_queue_manager *dqm,
 
 	retval = dqm->asic_ops.update_qpd(dqm, qpd);
 
-	dqm->processes_count++;
+	if (dqm->processes_count++ == 0)
+		dqm->dev->kfd2kgd->set_compute_idle(dqm->dev->kgd, false);
 
 	mutex_unlock(&dqm->lock);
 
@@ -792,7 +793,9 @@ static int unregister_process(struct device_queue_manager *dqm,
 		if (qpd == cur->qpd) {
 			list_del(&cur->list);
 			kfree(cur);
-			dqm->processes_count--;
+			if (--dqm->processes_count == 0)
+				dqm->dev->kfd2kgd->set_compute_idle(
+					dqm->dev->kgd, true);
 			goto out;
 		}
 	}
