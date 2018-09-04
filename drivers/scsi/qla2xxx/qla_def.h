fee525afa80e23cc1f56c464903a97fd5b828a7c
Quinn Tran <quinn.tran@cavium.com>
2018-09-04 14:19:12 -0700

    scsi: qla2xxx: Add logic to detect ABTS hang and response completion
    [ Upstream commit 0691094ff3f2cfa357b9de9030b65eddc4dab29d ]
    
    ABTS error completion can trigger an exchange cleanup from the driver and
    another ABTS response will be generated.  This retry of ABTS response can
    cause loop between driver trying to send ABTS and firmware returning error.
    
    This patch fixes this issue by adding logic to check for unresolved exchanges
    and clean up before ABTS is retried.  This patch also addes the fix to use the
    same qpair as the ABTS completion for the terminatation of exchange as well as
    retry of ABTS response.
    
    Signed-off-by: Quinn Tran <quinn.tran@cavium.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@cavium.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -3345,6 +3345,9 @@ struct qla_qpair {
 	struct list_head hints_list;
 	struct list_head nvme_done_list;
 	uint16_t cpuid;
+	uint16_t retry_term_cnt;
+	uint32_t retry_term_exchg_addr;
+	uint64_t retry_term_jiff;
 	struct qla_tgt_counters tgt_counters;
 };
 
