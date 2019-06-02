83abab2fca8ab3a779c5e65b01d263bed24b01fc
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-06-02 16:12:49 +0900

    ALSA: firewire-tascam: update isochronous resources when starting packet streaming after bus reset
    [ Upstream commit a0c049a647fbf55e8e6338a5c1e9316f6fbeabb2 ]
    
    After bus reset, isochronous resource manager releases all of allocated
    isochronous resources. The nodes to restart packet streaming should
    request reallocation of the resources.
    
    However, between the bus-reset and invocation of 'struct fw_driver.update'
    handler, ALSA PCM application can detect this situation by XRUN because
    the target device cancelled to transmit packets once bus-reset occurs.
    
    Due to the above mechanism, ALSA firewire-tascam driver just stops
    packet streaming in the update handler, thus pcm.prepare handler
    should request the reallocation.
    
    This commit requests the reallocation in pcm.prepare callback when
    bus generation is changed.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/firewire/tascam/tascam-stream.c
+++ sound/firewire/tascam/tascam-stream.c
@@ -335,7 +335,7 @@ int snd_tscm_stream_init_duplex(struct snd_tscm *tscm)
 	return err;
 }
 
-/* At bus reset, streaming is stopped and some registers are clear. */
+// At bus reset, streaming is stopped and some registers are clear.
 void snd_tscm_stream_update_duplex(struct snd_tscm *tscm)
 {
 	amdtp_stream_pcm_abort(&tscm->tx_stream);
@@ -404,6 +404,7 @@ void snd_tscm_stream_release_duplex(struct snd_tscm *tscm)
 
 int snd_tscm_stream_start_duplex(struct snd_tscm *tscm, unsigned int rate)
 {
+	unsigned int generation = tscm->rx_resources.generation;
 	int err;
 
 	if (tscm->substreams_counter == 0)
@@ -415,6 +416,16 @@ int snd_tscm_stream_start_duplex(struct snd_tscm *tscm, unsigned int rate)
 		amdtp_stream_stop(&tscm->tx_stream);
 
 		finish_session(tscm);
+	}
+
+	if (generation != fw_parent_device(tscm->unit)->card->generation) {
+		err = fw_iso_resources_update(&tscm->tx_resources);
+		if (err < 0)
+			goto error;
+
+		err = fw_iso_resources_update(&tscm->rx_resources);
+		if (err < 0)
+			goto error;
 	}
 
 	if (!amdtp_stream_running(&tscm->rx_stream)) {
