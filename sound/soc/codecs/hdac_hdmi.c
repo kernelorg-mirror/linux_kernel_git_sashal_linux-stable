3d7b00aab40a51a12d6bda6d3e366955449de8c6
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2019-07-02 16:24:26 +0300

    ASoC: hdac_hdmi: report codec link up/down status to bus
    [ Upstream commit e62bccf8a5ec47768ae2600dc880b148cce19f1b ]
    
    Report codec power status to the HDA codec bus from runtime pm
    suspend and resume callbacks. This is required to implement
    runtime idle logic that relies on 'codec_powered' field of hdac_bus
    to be maintained for all codecs.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Takashi Iwai <tiwai@suse.de>
    Link: https://lore.kernel.org/r/20190702132428.13129-2-kai.vehmanen@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/codecs/hdac_hdmi.c
+++ sound/soc/codecs/hdac_hdmi.c
@@ -2239,6 +2239,7 @@ static int hdac_hdmi_runtime_suspend(struct device *dev)
 		return -EIO;
 	}
 
+	snd_hdac_codec_link_down(hdev);
 	snd_hdac_ext_bus_link_put(bus, hlink);
 
 	err = snd_hdac_display_power(bus, false);
@@ -2268,6 +2269,7 @@ static int hdac_hdmi_runtime_resume(struct device *dev)
 	}
 
 	snd_hdac_ext_bus_link_get(bus, hlink);
+	snd_hdac_codec_link_up(hdev);
 
 	err = snd_hdac_display_power(bus, true);
 	if (err < 0) {
