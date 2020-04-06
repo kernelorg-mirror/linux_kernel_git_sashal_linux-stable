a02a726bf33e43fee600456018370a5d2d35592f
Bjorn Andersson <bjorn.andersson@linaro.org>
2020-04-05 17:32:29 -0700

    ASoC: qcom: common: Silence duplicate parse error messages
    [ Upstream commit d0c56b307f37fd21e2424d3c210e5d85831dd132 ]
    
    All error paths in qcom_snd_parse_of() prints more specific error
    messages, so silence the one in apq8096_platform_probe() and
    sdm845_snd_platform_probe() to avoid spamming the kernel log.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20200406003229.2354631-1-bjorn.andersson@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/qcom/apq8096.c
+++ sound/soc/qcom/apq8096.c
@@ -117,10 +117,8 @@ static int apq8096_platform_probe(struct platform_device *pdev)
 	card->owner = THIS_MODULE;
 	dev_set_drvdata(dev, card);
 	ret = qcom_snd_parse_of(card);
-	if (ret) {
-		dev_err(dev, "Error parsing OF data\n");
+	if (ret)
 		goto err;
-	}
 
 	apq8096_add_be_ops(card);
 	ret = snd_soc_register_card(card);
