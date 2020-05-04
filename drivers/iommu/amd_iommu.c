a22e99184c5e5bef0dc176bf4bf98bb0ede2c64a
Joerg Roedel <jroedel@suse.de>
2020-05-04 14:54:11 +0200

    iommu/amd: Call domain_flush_complete() in update_domain()
    [ Upstream commit f44a4d7e4f1cdef73c90b1dc749c4d8a7372a8eb ]
    
    The update_domain() function is expected to also inform the hardware
    about domain changes. This needs a COMPLETION_WAIT command to be sent
    to all IOMMUs which use the domain.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Tested-by: Qian Cai <cai@lca.pw>
    Link: https://lore.kernel.org/r/20200504125413.16798-4-joro@8bytes.org
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/iommu/amd_iommu.c
+++ drivers/iommu/amd_iommu.c
@@ -2344,6 +2344,7 @@ static void update_domain(struct protection_domain *domain)
 
 	/* Flush domain TLB(s) and wait for completion */
 	domain_flush_tlb_pde(domain);
+	domain_flush_complete(domain);
 }
 
 int __init amd_iommu_init_api(void)
