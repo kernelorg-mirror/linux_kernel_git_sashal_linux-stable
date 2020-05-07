486c8bdb10ea91794aec0802d75e06a8417ccaa7
Douglas Anderson <dianders@chromium.org>
2020-05-07 13:08:45 -0700

    kgdboc: Remove useless #ifdef CONFIG_KGDB_SERIAL_CONSOLE in kgdboc
    [ Upstream commit eae3e19ca930a56c726fbf4dc3adc198b8f5d61d ]
    
    This file is only ever compiled if that config is on since the
    Makefile says:
    
      obj-$(CONFIG_KGDB_SERIAL_CONSOLE) += kgdboc.o
    
    Let's get rid of the useless #ifdef.
    
    Reported-by: Daniel Thompson <daniel.thompson@linaro.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/20200507130644.v4.7.Icb528f03d0026d957e60f537aa711ada6fd219dc@changeid
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/tty/serial/kgdboc.c
+++ drivers/tty/serial/kgdboc.c
@@ -384,7 +384,6 @@ static struct kgdb_io kgdboc_io_ops = {
 	.post_exception		= kgdboc_post_exp_handler,
 };
 
-#ifdef CONFIG_KGDB_SERIAL_CONSOLE
 static int kgdboc_option_setup(char *opt)
 {
 	if (!opt) {
@@ -413,7 +412,6 @@ static int __init kgdboc_early_init(char *opt)
 }
 
 early_param("ekgdboc", kgdboc_early_init);
-#endif /* CONFIG_KGDB_SERIAL_CONSOLE */
 
 module_init(init_kgdboc);
 module_exit(exit_kgdboc);
