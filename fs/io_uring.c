34d8418b0cc130794dda305c6da2779d32190075
Pavel Begunkov <asml.silence@gmail.com>
2020-05-30 14:19:15 +0300

    io_uring: fix overflowed reqs cancellation
    [ Upstream commit 7b53d59859bc932b37895d2d37388e7fa29af7a5 ]
    
    Overflowed requests in io_uring_cancel_files() should be shed only of
    inflight and overflowed refs. All other left references are owned by
    someone else.
    
    If refcount_sub_and_test() fails, it will go further and put put extra
    ref, don't do that. Also, don't need to do io_wq_cancel_work()
    for overflowed reqs, they will be let go shortly anyway.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -6526,10 +6526,11 @@ static void io_uring_cancel_files(struct io_ring_ctx *ctx,
 				io_put_req(cancel_req);
 				continue;
 			}
+		} else {
+			io_wq_cancel_work(ctx->io_wq, &cancel_req->work);
+			io_put_req(cancel_req);
 		}
 
-		io_wq_cancel_work(ctx->io_wq, &cancel_req->work);
-		io_put_req(cancel_req);
 		schedule();
 	}
 	finish_wait(&ctx->inflight_wait, &wait);
