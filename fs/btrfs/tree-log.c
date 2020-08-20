f3c5a235b42a12c84c0c8257f92ca7bb88bc08fd
Josef Bacik <josef@toxicpanda.com>
2020-08-20 11:46:10 -0400

    btrfs: locking: remove all the blocking helpers
    [ Upstream commit ac5887c8e013d6754d36e6d51dc03448ee0b0065 ]
    
    Now that we're using a rw_semaphore we no longer need to indicate if a
    lock is blocking or not, nor do we need to flip the entire path from
    blocking to spinning.  Remove these helpers and all the places they are
    called.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/tree-log.c
+++ fs/btrfs/tree-log.c
@@ -2777,7 +2777,6 @@ static noinline int walk_down_log_tree(struct btrfs_trans_handle *trans,
 
 				if (trans) {
 					btrfs_tree_lock(next);
-					btrfs_set_lock_blocking_write(next);
 					btrfs_clean_tree_block(next);
 					btrfs_wait_tree_block_writeback(next);
 					btrfs_tree_unlock(next);
@@ -2846,7 +2845,6 @@ static noinline int walk_up_log_tree(struct btrfs_trans_handle *trans,
 
 				if (trans) {
 					btrfs_tree_lock(next);
-					btrfs_set_lock_blocking_write(next);
 					btrfs_clean_tree_block(next);
 					btrfs_wait_tree_block_writeback(next);
 					btrfs_tree_unlock(next);
@@ -2928,7 +2926,6 @@ static int walk_log_tree(struct btrfs_trans_handle *trans,
 
 			if (trans) {
 				btrfs_tree_lock(next);
-				btrfs_set_lock_blocking_write(next);
 				btrfs_clean_tree_block(next);
 				btrfs_wait_tree_block_writeback(next);
 				btrfs_tree_unlock(next);
