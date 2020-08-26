997eed5ff121764168227313e2d3674b09eb2ea5
Lyude Paul <lyude@redhat.com>
2020-08-26 14:24:47 -0400

    drm/nouveau/kms: Move drm_dp_cec_unset_edid() into nouveau_connector_detect()
    [ Upstream commit 02bb7fe2d3eacb5ca1cec597ad16eff93ae8d769 ]
    
    For whatever reason we currently unset the EDID for DP CEC support when
    responding to the connector being unplugged, instead of just doing it in
    nouveau_connector_detect() where we set the CEC EDID. This isn't really
    needed and could even potentially cause us to forget to unset the EDID
    if the connector is removed without a corresponding hpd event, so let's
    fix that.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Ben Skeggs <bskeggs@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200826182456.322681-12-lyude@redhat.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/nouveau/nouveau_connector.c
+++ drivers/gpu/drm/nouveau/nouveau_connector.c
@@ -643,10 +643,11 @@ detect_analog:
 			conn_status = connector_status_connected;
 			goto out;
 		}
-
 	}
 
  out:
+	if (!nv_connector->edid)
+		drm_dp_cec_unset_edid(&nv_connector->aux);
 
 	pm_runtime_mark_last_busy(dev->dev);
 	pm_runtime_put_autosuspend(dev->dev);
@@ -1177,8 +1178,6 @@ nouveau_connector_hotplug(struct nvif_notify *notify)
 		return NVIF_NOTIFY_DROP;
 	}
 
-	if (!plugged)
-		drm_dp_cec_unset_edid(&nv_connector->aux);
 	NV_DEBUG(drm, "%splugged %s\n", plugged ? "" : "un", name);
 	if ((nv_encoder = find_encoder(connector, DCB_OUTPUT_DP))) {
 		if (!plugged)
