dd6de07ed1d06bea3a28ee77e66af75672ba3b2b
Marc Zyngier <maz@kernel.org>
2020-11-20 09:42:05 +0000

    drm/meson: dw-hdmi: Enable the iahb clock early enough
    [ Upstream commit 2b6cb81b95d1e8abfb6d32cf194a5bd2992c315c ]
    
    Instead of moving meson_dw_hdmi_init() around which breaks existing
    platform, let's enable the clock meson_dw_hdmi_init() depends on.
    This means we don't have to worry about this clock being enabled or
    not, depending on the boot-loader features.
    
    Fixes: b33340e33acd ("drm/meson: dw-hdmi: Ensure that clocks are enabled before touching the TOP registers")
    Reported-by: "kernelci.org bot" <bot@kernelci.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Tested-by: Guillaume Tucker <guillaume.tucker@collabora.com>
    Reviewed-by: Neil Armstrong <narmstrong@baylibre.com>
    [narmstrong: changed reported by to kernelci.org bot]
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201120094205.525228-3-maz@kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/meson/meson_dw_hdmi.c
+++ drivers/gpu/drm/meson/meson_dw_hdmi.c
@@ -991,6 +991,10 @@ static int meson_dw_hdmi_bind(struct device *dev, struct device *master,
 	if (ret)
 		return ret;
 
+	ret = meson_enable_clk(dev, "iahb");
+	if (ret)
+		return ret;
+
 	ret = meson_enable_clk(dev, "venci");
 	if (ret)
 		return ret;
@@ -1028,6 +1032,8 @@ static int meson_dw_hdmi_bind(struct device *dev, struct device *master,
 
 	encoder->possible_crtcs = BIT(0);
 
+	meson_dw_hdmi_init(meson_dw_hdmi);
+
 	DRM_DEBUG_DRIVER("encoder initialized\n");
 
 	/* Bridge / Connector */
@@ -1045,8 +1051,6 @@ static int meson_dw_hdmi_bind(struct device *dev, struct device *master,
 					    &meson_dw_hdmi->dw_plat_data);
 	if (IS_ERR(meson_dw_hdmi->hdmi))
 		return PTR_ERR(meson_dw_hdmi->hdmi);
-
-	meson_dw_hdmi_init(meson_dw_hdmi);
 
 	next_bridge = of_drm_find_bridge(pdev->dev.of_node);
 	if (next_bridge)
