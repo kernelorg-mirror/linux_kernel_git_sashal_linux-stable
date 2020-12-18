c3e93e86d60d708d7b42dfba35266a9d9f89e2aa
Xiaojian Du <Xiaojian.Du@amd.com>
2020-12-18 16:07:14 +0800

    drm/amd/pm: enable the fine grain tuning function for renoir
    [ Upstream commit 16a0fd2a15f4735e5523fa9794e39d6176e5040a ]
    
    This patch is to enable the fine grain tuning function for renoir.
    
    Signed-off-by: Xiaojian Du <Xiaojian.Du@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -400,6 +400,8 @@ static int smu_set_funcs(struct amdgpu_device *adev)
 		break;
 	case CHIP_RENOIR:
 		renoir_set_ppt_funcs(smu);
+		/* enable the OD by default to allow the fine grain tuning function */
+		smu->od_enabled = true;
 		break;
 	default:
 		return -EINVAL;
