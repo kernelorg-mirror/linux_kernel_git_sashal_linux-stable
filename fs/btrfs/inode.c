bc6cd7d8d09adad9aad09e9682fc585770104381
Ira Weiny <ira.weiny@intel.com>
2021-02-16 18:48:23 -0800

    btrfs: convert kmap to kmap_local_page, simple cases
    [ Upstream commit 58c1a35cd52268ae1524b3ff5eac9fa2414e6efc ]
    
    Use a simple coccinelle script to help convert the most common
    kmap()/kunmap() patterns to kmap_local_page()/kunmap_local().
    
    Note that some kmaps which were caught by this script needed to be
    handled by hand because of the strict unmapping order of kunmap_local()
    so they are not included in this patch.  But this script got us started.
    
    There's another temp variable added for the final length write to the
    first page so it does not interfere with cpage_out that is used for
    mapping other pages.
    
    The development of this patch was aided by the follow script:
    
    // <smpl>
    // SPDX-License-Identifier: GPL-2.0-only
    // Find kmap and replace with kmap_local_page then mark kunmap
    //
    // Confidence: Low
    // Copyright: (C) 2021 Intel Corporation
    // URL: http://coccinelle.lip6.fr/
    
    @ catch_all @
    expression e, e2;
    @@
    
    (
    -kmap(e)
    +kmap_local_page(e)
    )
    ...
    (
    -kunmap(...)
    +kunmap_local()
    )
    
    // </smpl>
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6765,7 +6765,7 @@ next:
 				if (ret)
 					goto out;
 			} else {
-				map = kmap(page);
+				map = kmap_local_page(page);
 				read_extent_buffer(leaf, map + pg_offset, ptr,
 						   copy_size);
 				if (pg_offset + copy_size < PAGE_SIZE) {
@@ -6773,7 +6773,7 @@ next:
 					       PAGE_SIZE - pg_offset -
 					       copy_size);
 				}
-				kunmap(page);
+				kunmap_local(map);
 			}
 			flush_dcache_page(page);
 		}
