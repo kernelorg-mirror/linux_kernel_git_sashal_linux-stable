e8920fa34b878b50a17731542c7ba9d4bde6d211
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2021-02-18 16:19:16 -0600

    ASoC: soc-ops: remove useless assignment
    [ Upstream commit 7ca5f04946176ae9611bb80e3ec3d3fa300b7cff ]
    
    cppcheck warning:
    
    sound/soc/soc-ops.c:410:35: style: Variable 'val2' is assigned a value
    that is never used. [unreadVariable]
     unsigned int val, val_mask, val2 = 0;
                                      ^
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20210218221921.88991-2-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/soc-ops.c
+++ sound/soc/soc-ops.c
@@ -427,7 +427,7 @@ int snd_soc_put_volsw_sx(struct snd_kcontrol *kcontrol,
 	int min = mc->min;
 	unsigned int mask = (1U << (fls(min + max) - 1)) - 1;
 	int err = 0;
-	unsigned int val, val_mask, val2 = 0;
+	unsigned int val, val_mask, val2;
 
 	val = ucontrol->value.integer.value[0];
 	if (mc->platform_max && val > mc->platform_max)
