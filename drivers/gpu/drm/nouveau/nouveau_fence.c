74fb3f0f2ddeaad8315b270be347359b4181f376
Christian König <christian.koenig@amd.com>
2021-06-06 11:50:15 +0200

    drm/nouveau: always wait for the exclusive fence
    [ Upstream commit 3e1ad79bf66165bdb2baca3989f9227939241f11 ]
    
    Drivers also need to to sync to the exclusive fence when
    a shared one is present.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210702111642.17259-4-christian.koenig@amd.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/nouveau/nouveau_fence.c
+++ drivers/gpu/drm/nouveau/nouveau_fence.c
@@ -358,7 +358,7 @@ nouveau_fence_sync(struct nouveau_bo *nvbo, struct nouveau_channel *chan, bool e
 	fobj = dma_resv_get_list(resv);
 	fence = dma_resv_get_excl(resv);
 
-	if (fence && (!exclusive || !fobj || !fobj->shared_count)) {
+	if (fence) {
 		struct nouveau_channel *prev = NULL;
 		bool must_wait = true;
 
