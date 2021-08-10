7d70f99ed79642123ff09640efe8ed2bf1f1e5e3
Krishna Reddy <vdumpa@nvidia.com>
2021-08-10 10:14:01 +0530

    iommu/arm-smmu: Fix race condition during iommu_group creation
    [ Upstream commit b1a1347912a742a4e1fcdc9df6302dd9dd2c3405 ]
    
    When two devices with same SID are getting probed concurrently through
    iommu_probe_device(), the iommu_group sometimes is getting allocated more
    than once as call to arm_smmu_device_group() is not protected for
    concurrency. Furthermore, it leads to each device holding a different
    iommu_group and domain pointer, separate IOVA space and only one of the
    devices' domain is used for translations from IOMMU. This causes accesses
    from other device to fault or see incorrect translations.
    Fix this by protecting iommu_group allocation from concurrency in
    arm_smmu_device_group().
    
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Signed-off-by: Ashish Mhetre <amhetre@nvidia.com>
    Link: https://lore.kernel.org/r/1628570641-9127-3-git-send-email-amhetre@nvidia.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1462,6 +1462,7 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 	struct iommu_group *group = NULL;
 	int i, idx;
 
+	mutex_lock(&smmu->stream_map_mutex);
 	for_each_cfg_sme(cfg, fwspec, i, idx) {
 		if (group && smmu->s2crs[idx].group &&
 		    group != smmu->s2crs[idx].group)
@@ -1470,8 +1471,10 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 		group = smmu->s2crs[idx].group;
 	}
 
-	if (group)
+	if (group) {
+		mutex_unlock(&smmu->stream_map_mutex);
 		return iommu_group_ref_get(group);
+	}
 
 	if (dev_is_pci(dev))
 		group = pci_device_group(dev);
@@ -1485,6 +1488,7 @@ static struct iommu_group *arm_smmu_device_group(struct device *dev)
 		for_each_cfg_sme(cfg, fwspec, i, idx)
 			smmu->s2crs[idx].group = group;
 
+	mutex_unlock(&smmu->stream_map_mutex);
 	return group;
 }
 
