3058370f198e20c7c05fe0ca2ffeafb9d58fa477
Ivan T. Ivanov <iivanov@suse.de>
2021-11-16 10:46:16 +0200

    ARM: rockchip: Use memcpy_toio instead of memcpy on smp bring-up
    [ Upstream commit 423e85e97aaf69e5198bbec6811e3825c8b5019a ]
    
    This fixes a potential kernel panic on memcpy when FORTIFY_SOURCE
    is enabled. Because memory is iomem use appropriate function for
    accessing it.
    
    Signed-off-by: Ivan T. Ivanov <iivanov@suse.de>
    Link: https://lore.kernel.org/r/20211116084616.24811-1-iivanov@suse.de
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm/mach-rockchip/platsmp.c
+++ arch/arm/mach-rockchip/platsmp.c
@@ -194,7 +194,7 @@ static int __init rockchip_smp_prepare_sram(struct device_node *node)
 	rockchip_boot_fn = virt_to_phys(secondary_startup);
 
 	/* copy the trampoline to sram, that runs during startup of the core */
-	memcpy(sram_base_addr, &rockchip_secondary_trampoline, trampoline_sz);
+	memcpy_toio(sram_base_addr, &rockchip_secondary_trampoline, trampoline_sz);
 	flush_cache_all();
 	outer_clean_range(0, trampoline_sz);
 
