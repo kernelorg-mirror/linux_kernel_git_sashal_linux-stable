f48d15c6d7e25f2ec359d1d8191ce71fcb031cac
Lai Jiangshan <laijs@linux.alibaba.com>
2021-12-07 17:52:30 +0800

    KVM: X86: Raise #GP when clearing CR0_PG in 64 bit mode
    [ Upstream commit 777ab82d7ce0451fd47bb57e331548deba57394e ]
    
    In the SDM:
    If the logical processor is in 64-bit mode or if CR4.PCIDE = 1, an
    attempt to clear CR0.PG causes a general-protection exception (#GP).
    Software should transition to compatibility mode and clear CR4.PCIDE
    before attempting to disable paging.
    
    Signed-off-by: Lai Jiangshan <laijs@linux.alibaba.com>
    Message-Id: <20211207095230.53437-1-jiangshanlai@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -633,7 +633,8 @@ int kvm_set_cr0(struct kvm_vcpu *vcpu, unsigned long cr0)
 			return 1;
 	}
 
-	if (!(cr0 & X86_CR0_PG) && kvm_read_cr4_bits(vcpu, X86_CR4_PCIDE))
+	if (!(cr0 & X86_CR0_PG) &&
+	    (is_64_bit_mode(vcpu) || kvm_read_cr4_bits(vcpu, X86_CR4_PCIDE)))
 		return 1;
 
 	kvm_x86_ops->set_cr0(vcpu, cr0);
