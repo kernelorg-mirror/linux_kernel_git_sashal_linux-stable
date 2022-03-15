4f986ac3ca4446d6593e2ea891ac2f1cd656af27
Roman Li <Roman.Li@amd.com>
2022-03-15 16:31:14 -0400

    drm/amd/display: Enable power gating before init_pipes
    [ Upstream commit 58e16c752e9540b28a873c44c3bee83e022007c1 ]
    
    [Why]
    In init_hw() we call init_pipes() before enabling power gating.
    init_pipes() tries to power gate dsc but it may fail because
    required force-ons are not released yet.
    As a result with dsc config the following errors observed on resume:
    "REG_WAIT timeout 1us * 1000 tries - dcn20_dsc_pg_control"
    "REG_WAIT timeout 1us * 1000 tries - dcn20_dpp_pg_control"
    "REG_WAIT timeout 1us * 1000 tries - dcn20_hubp_pg_control"
    
    [How]
    Move enable_power_gating_plane() before init_pipes() in init_hw()
    
    Reviewed-by: Anthony Koo <Anthony.Koo@amd.com>
    Reviewed-by: Eric Yang <Eric.Yang2@amd.com>
    Acked-by: Alex Hung <alex.hung@amd.com>
    Signed-off-by: Roman Li <Roman.Li@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hw_sequencer.c
@@ -1506,6 +1506,9 @@ void dcn10_init_hw(struct dc *dc)
 		}
 	}
 
+	if (hws->funcs.enable_power_gating_plane)
+		hws->funcs.enable_power_gating_plane(dc->hwseq, true);
+
 	/* If taking control over from VBIOS, we may want to optimize our first
 	 * mode set, so we need to skip powering down pipes until we know which
 	 * pipes we want to use.
@@ -1558,8 +1561,6 @@ void dcn10_init_hw(struct dc *dc)
 
 		REG_UPDATE(DCFCLK_CNTL, DCFCLK_GATE_DIS, 0);
 	}
-	if (hws->funcs.enable_power_gating_plane)
-		hws->funcs.enable_power_gating_plane(dc->hwseq, true);
 
 	if (dc->clk_mgr->funcs->notify_wm_ranges)
 		dc->clk_mgr->funcs->notify_wm_ranges(dc->clk_mgr);
