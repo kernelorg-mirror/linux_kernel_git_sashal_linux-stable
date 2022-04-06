7864795bede2594a85915f4b219dc94f4a85cb0c
Oliver Upton <oupton@google.com>
2022-04-06 23:56:13 +0000

    KVM: Don't create VM debugfs files outside of the VM directory
    [ Upstream commit a44a4cc1c969afec97dbb2aedaf6f38eaa6253bb ]
    
    Unfortunately, there is no guarantee that KVM was able to instantiate a
    debugfs directory for a particular VM. To that end, KVM shouldn't even
    attempt to create new debugfs files in this case. If the specified
    parent dentry is NULL, debugfs_create_file() will instantiate files at
    the root of debugfs.
    
    For arm64, it is possible to create the vgic-state file outside of a
    VM directory, the file is not cleaned up when a VM is destroyed.
    Nonetheless, the corresponding struct kvm is freed when the VM is
    destroyed.
    
    Nip the problem in the bud for all possible errant debugfs file
    creations by initializing kvm->debugfs_dentry to -ENOENT. In so doing,
    debugfs_create_file() will fail instead of creating the file in the root
    directory.
    
    Cc: stable@kernel.org
    Fixes: 929f45e32499 ("kvm: no need to check return value of debugfs_create functions")
    Signed-off-by: Oliver Upton <oupton@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20220406235615.1447180-2-oupton@google.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -699,6 +699,12 @@ static int kvm_create_vm_debugfs(struct kvm *kvm, int fd)
 	 */
 	kvm->debugfs_dentry = ERR_PTR(-ENOENT);
 
+	/*
+	 * Force subsequent debugfs file creations to fail if the VM directory
+	 * is not created.
+	 */
+	kvm->debugfs_dentry = ERR_PTR(-ENOENT);
+
 	if (!debugfs_initialized())
 		return 0;
 
