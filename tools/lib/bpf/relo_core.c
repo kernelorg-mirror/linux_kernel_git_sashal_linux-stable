265d6d835c4af1d53a42c4d1581de3bd00cdb420
Yonghong Song <yhs@fb.com>
2022-06-06 23:26:10 -0700

    libbpf: Fix an error in 64bit relocation value computation
    [ Upstream commit b58b2b3a31228bd9aaed9b96e9452dafd0d46024 ]
    
    Currently, the 64bit relocation value in the instruction
    is computed as follows:
      __u64 imm = insn[0].imm + ((__u64)insn[1].imm << 32)
    
    Suppose insn[0].imm = -1 (0xffffffff) and insn[1].imm = 1.
    With the above computation, insn[0].imm will first sign-extend
    to 64bit -1 (0xffffffffFFFFFFFF) and then add 0x1FFFFFFFF,
    producing incorrect value 0xFFFFFFFF. The correct value
    should be 0x1FFFFFFFF.
    
    Changing insn[0].imm to __u32 first will prevent 64bit sign
    extension and fix the issue. Merging high and low 32bit values
    also changed from '+' to '|' to be consistent with other
    similar occurences in kernel and libbpf.
    
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: Dave Marchevsky <davemarchevsky@fb.com>
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/r/20220607062610.3717378-1-yhs@fb.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/lib/bpf/relo_core.c
+++ tools/lib/bpf/relo_core.c
@@ -1025,7 +1025,7 @@ poison:
 			return -EINVAL;
 		}
 
-		imm = insn[0].imm + ((__u64)insn[1].imm << 32);
+		imm = (__u32)insn[0].imm | ((__u64)insn[1].imm << 32);
 		if (res->validate && imm != orig_val) {
 			pr_warn("prog '%s': relo #%d: unexpected insn #%d (LDIMM64) value: got %llu, exp %u -> %u\n",
 				prog_name, relo_idx,
