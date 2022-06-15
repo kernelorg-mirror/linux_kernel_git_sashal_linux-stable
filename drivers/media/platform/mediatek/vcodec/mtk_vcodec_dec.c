809e82dc7c971e037f0aba1ec297ac7482d2726d
Justin Green <greenjustin@chromium.org>
2022-06-15 16:38:27 +0100

    media: mediatek: vcodec: return EINVAL if plane is too small
    [ Upstream commit f5caaa47f55fa742f1a230e5b4258c139e223c74 ]
    
    Modify vb2ops_vdec_buf_prepare to return EINVAL if the size of the plane
    is less than the size of the image. Currently we just log an error and
    return 0 anyway, which may cause a buffer overrun bug.
    
    Signed-off-by: Justin Green <greenjustin@chromium.org>
    Suggested-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/media/platform/mediatek/vcodec/mtk_vcodec_dec.c
+++ drivers/media/platform/mediatek/vcodec/mtk_vcodec_dec.c
@@ -725,6 +725,7 @@ int vb2ops_vdec_buf_prepare(struct vb2_buffer *vb)
 			mtk_v4l2_err("data will not fit into plane %d (%lu < %d)",
 				i, vb2_plane_size(vb, i),
 				q_data->sizeimage[i]);
+			return -EINVAL;
 		}
 	}
 
