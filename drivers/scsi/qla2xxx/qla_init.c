fd0dfda26da93d2feef916014cf80324431c6f77
Bikash Hazarika <bhazarika@marvell.com>
2022-07-12 22:20:37 -0700

    scsi: qla2xxx: Fix incorrect display of max frame size
    [ Upstream commit cf3b4fb655796674e605268bd4bfb47a47c8bce6 ]
    
    Replace display field with the correct field.
    
    Link: https://lore.kernel.org/r/20220713052045.10683-3-njavali@marvell.com
    Fixes: 8777e4314d39 ("scsi: qla2xxx: Migrate NVME N2N handling into state machine")
    Cc: stable@vger.kernel.org
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Signed-off-by: Bikash Hazarika <bhazarika@marvell.com>
    Signed-off-by: Nilesh Javali <njavali@marvell.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/qla2xxx/qla_init.c
+++ drivers/scsi/qla2xxx/qla_init.c
@@ -4184,6 +4184,8 @@ qla2x00_init_rings(scsi_qla_host_t *vha)
 		    (mid_init_cb->init_cb.firmware_options_1 & BIT_6) != 0;
 		ql_dbg(ql_dbg_init, vha, 0x00bc, "FA-WWPN Support: %s.\n",
 		    (ha->flags.fawwpn_enabled) ? "enabled" : "disabled");
+		/* Init_cb will be reused for other command(s).  Save a backup copy of port_name */
+		memcpy(ha->port_name, ha->init_cb->port_name, WWN_SIZE);
 	}
 
 	rval = qla2x00_init_firmware(vha, ha->init_cb_size);
