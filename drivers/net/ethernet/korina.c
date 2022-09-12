0bc2e5db1a45f158f20787c43795550890373123
Nathan Huckleberry <nhuck@google.com>
2022-09-12 14:43:40 -0700

    net: korina: Fix return type of korina_send_packet
    [ Upstream commit 106c67ce46f3c82dd276e983668a91d6ed631173 ]
    
    The ndo_start_xmit field in net_device_ops is expected to be of type
    netdev_tx_t (*ndo_start_xmit)(struct sk_buff *skb, struct net_device *dev).
    
    The mismatched return type breaks forward edge kCFI since the underlying
    function definition does not match the function hook definition.
    
    The return type of korina_send_packet should be changed from int to
    netdev_tx_t.
    
    Reported-by: Dan Carpenter <error27@gmail.com>
    Link: https://github.com/ClangBuiltLinux/linux/issues/1703
    Cc: llvm@lists.linux.dev
    Signed-off-by: Nathan Huckleberry <nhuck@google.com>
    Reviewed-by: Nathan Chancellor <nathan@kernel.org>
    Link: https://lore.kernel.org/r/20220912214344.928925-1-nhuck@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/korina.c
+++ drivers/net/ethernet/korina.c
@@ -193,7 +193,8 @@ static void korina_chain_rx(struct korina_private *lp,
 }
 
 /* transmit packet */
-static int korina_send_packet(struct sk_buff *skb, struct net_device *dev)
+static netdev_tx_t korina_send_packet(struct sk_buff *skb,
+				      struct net_device *dev)
 {
 	struct korina_private *lp = netdev_priv(dev);
 	unsigned long flags;
