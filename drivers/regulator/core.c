f33d930050868132cf80a21dbaaaf930b2020b25
ChiYuan Huang <cy_huang@richtek.com>
2022-12-06 15:22:21 +0800

    regulator: core: Use different devices for resource allocation and DT lookup
    [ Upstream commit 8f3cbcd6b440032ebc7f7d48a1689dcc70a4eb98 ]
    
    Following by the below discussion, there's the potential UAF issue
    between regulator and mfd.
    https://lore.kernel.org/all/20221128143601.1698148-1-yangyingliang@huawei.com/
    
    From the analysis of Yingliang
    
    CPU A				|CPU B
    mt6370_probe()			|
      devm_mfd_add_devices()	|
    				|mt6370_regulator_probe()
    				|  regulator_register()
    				|    //allocate init_data and add it to devres
    				|    regulator_of_get_init_data()
    i2c_unregister_device()		|
      device_del()			|
        devres_release_all()	|
          // init_data is freed	|
          release_nodes()		|
    				|  // using init_data causes UAF
    				|  regulator_register()
    
    It's common to use mfd core to create child device for the regulator.
    In order to do the DT lookup for init data, the child that registered
    the regulator would pass its parent as the parameter. And this causes
    init data resource allocated to its parent, not itself. The issue happen
    when parent device is going to release and regulator core is still doing
    some operation of init data constraint for the regulator of child device.
    
    To fix it, this patch expand 'regulator_register' API to use the
    different devices for init data allocation and DT lookup.
    
    Reported-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: ChiYuan Huang <cy_huang@richtek.com>
    Link: https://lore.kernel.org/r/1670311341-32664-1-git-send-email-u0084500@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -5386,6 +5386,7 @@ static struct regulator_coupler generic_regulator_coupler = {
 
 /**
  * regulator_register - register regulator
+ * @dev: the device that drive the regulator
  * @regulator_desc: regulator to register
  * @cfg: runtime configuration for regulator
  *
@@ -5394,7 +5395,8 @@ static struct regulator_coupler generic_regulator_coupler = {
  * or an ERR_PTR() on error.
  */
 struct regulator_dev *
-regulator_register(const struct regulator_desc *regulator_desc,
+regulator_register(struct device *dev,
+		   const struct regulator_desc *regulator_desc,
 		   const struct regulator_config *cfg)
 {
 	const struct regulator_init_data *init_data;
@@ -5403,7 +5405,6 @@ regulator_register(const struct regulator_desc *regulator_desc,
 	struct regulator_dev *rdev;
 	bool dangling_cfg_gpiod = false;
 	bool dangling_of_gpiod = false;
-	struct device *dev;
 	int ret, i;
 
 	if (cfg == NULL)
@@ -5415,8 +5416,7 @@ regulator_register(const struct regulator_desc *regulator_desc,
 		goto rinse;
 	}
 
-	dev = cfg->dev;
-	WARN_ON(!dev);
+	WARN_ON(!dev || !cfg->dev);
 
 	if (regulator_desc->name == NULL || regulator_desc->ops == NULL) {
 		ret = -EINVAL;
