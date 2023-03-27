63bbf95d9fe5d7d1dc2ebb66a7b3c806fa7a8e4d
Moudy Ho <moudy.ho@mediatek.com>
2023-03-27 11:13:34 +0800

    media: platform: mtk-mdp3: decompose hardware-related information in shared memory
    [ Upstream commit 09e694f1e9da4fd9c00e268c41f02c8b8c31cc50 ]
    
    The communication between the MDP3 kernel driver and SCP is to
    pass a shared memory through the cooperation of "mtk-mdp3-vpu.c" and
    remoteproc driver.
    The data structure of this shared memory is defined in "mtk-img-ipi.h",
    as shown below:
    
    vpu->work_addr -> +-----------------------------------------+
                      |                                         |
                      | To SCP : Input frame parameters         |
                      |          (struct img_ipi_frameparam)    |
                      |                                         |
         vpu->pool -> +-----------------------------------------+
                      |                                         |
                      | From SCP : Output component config pool |
                      |            (struct img_config)          |
                      |                                         |
                      |           *struct img_config 1          |
                      |                    |                    |
                      |                    |                    |
                      |                    v                    |
                      |           *struct img_config N          |
                      |            (N = MDP_CONFIG_POOL_SIZE)   |
                      +-----------------------------------------+
    
    One output component configuration contains the components
    currently used by the pipeline, and has the register settings
    that each component needs to set.
    
    Since the quantity, type and function of components on each chip
    will vary, the effect is that the size of the "struct img_config"
    and its substructures will be different on each chip.
    In addition, all chips will have to update their SCP firmware for
    every change if the output component config structure is defined
    and shared by a common header.
    
    Therefore, all functions that operate on "struct img_config" and
    its substructures must be separated by chips and so are the
    relevant definations.
    
    Signed-off-by: Moudy Ho <moudy.ho@mediatek.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Stable-dep-of: 2a76e7679b59 ("media: platform: mtk-mdp3: fix uninitialized variable in mdp_path_config()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/media/platform/mediatek/mdp3/mdp_cfg_data.c
+++ drivers/media/platform/mediatek/mdp3/mdp_cfg_data.c
@@ -4,6 +4,7 @@
  * Author: Ping-Hsun Wu <ping-hsun.wu@mediatek.com>
  */
 
+#include "mtk-img-ipi.h"
 #include "mtk-mdp3-cfg.h"
 #include "mtk-mdp3-core.h"
 #include "mtk-mdp3-comp.h"
@@ -145,6 +146,7 @@ static const struct mdp_comp_data mt8183_mdp_comp_data[MDP_MAX_COMP_COUNT] = {
 };
 
 const struct mtk_mdp_driver_data mt8183_mdp_driver_data = {
+	.mdp_plat_id = MT8183,
 	.mdp_probe_infra = mt8183_mdp_probe_infra,
 	.mdp_cfg = &mt8183_plat_cfg,
 	.mdp_mutex_table_idx = mt8183_mutex_idx,
