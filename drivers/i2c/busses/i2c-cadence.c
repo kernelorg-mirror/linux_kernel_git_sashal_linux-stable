20a7ef69d0e10f1337de959d72f18efaa97daefb
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2023-05-08 22:51:38 +0200

    i2c: Convert to platform remove callback returning void
    [ Upstream commit e190a0c389e60178fba3d532abf936dcae223e7d ]
    
    The .remove() callback for a platform driver returns an int which makes
    many driver authors wrongly assume it's possible to do error handling by
    returning an error code. However the value returned is (mostly) ignored
    and this typically results in resource leaks. To improve here there is a
    quest to make the remove callback return void. In the first step of this
    quest all drivers are converted to .remove_new() which already returns
    void.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Alain Volmat <alain.volmat@foss.st.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Acked-by: Baruch Siach <baruch@tkos.co.il>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Heiko Stuebner <heiko@sntech.de>
    Acked-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Acked-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Acked-by: Jochen Friedrich <jochen@scram.de>
    Acked-by: Peter Rosin <peda@axentia.se>
    Acked-by: Vadim Pasternak <vadimp@nvidia.com>
    Reviewed-by: Asmaa Mnebhi <asnaa@nvidia.com>
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
    Reviewed-by: Chris Packham <chris.packham@alliedtelesis.co.nz>
    Reviewed-by: Chris Pringle <chris.pringle@phabrix.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Jean Delvare <jdelvare@suse.de>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@linaro.org>
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Reviewed-by: Matthias Brugger <matthias.bgg@gmail.com>
    Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Reviewed-by: Tali Perry <tali.perry@nuvoton.com>
    Reviewed-by: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Wolfram Sang <wsa@kernel.org>
    Stable-dep-of: 9e1a1ee93f6b ("i2c: ocores: use devm_ managed clks")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/i2c/busses/i2c-cadence.c
+++ drivers/i2c/busses/i2c-cadence.c
@@ -1359,7 +1359,7 @@ err_clk_dis:
  *
  * Return: 0 always
  */
-static int cdns_i2c_remove(struct platform_device *pdev)
+static void cdns_i2c_remove(struct platform_device *pdev)
 {
 	struct cdns_i2c *id = platform_get_drvdata(pdev);
 
@@ -1370,8 +1370,6 @@ static int cdns_i2c_remove(struct platform_device *pdev)
 	i2c_del_adapter(&id->adap);
 	clk_notifier_unregister(id->clk, &id->clk_rate_change_nb);
 	clk_disable_unprepare(id->clk);
-
-	return 0;
 }
 
 static struct platform_driver cdns_i2c_drv = {
@@ -1381,7 +1379,7 @@ static struct platform_driver cdns_i2c_drv = {
 		.pm = &cdns_i2c_dev_pm_ops,
 	},
 	.probe  = cdns_i2c_probe,
-	.remove = cdns_i2c_remove,
+	.remove_new = cdns_i2c_remove,
 };
 
 module_platform_driver(cdns_i2c_drv);
