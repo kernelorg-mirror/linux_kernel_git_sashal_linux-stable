8e29c07cd693a92ae604d975b65ad6ee45861e5e
Manas Ghandat <ghandatmanas@gmail.com>
2023-10-11 20:09:37 +0530

    jfs: fix shift-out-of-bounds in dbJoin
    [ Upstream commit cca974daeb6c43ea971f8ceff5a7080d7d49ee30 ]
    
    Currently while joining the leaf in a buddy system there is shift out
    of bound error in calculation of BUDSIZE. Added the required check
    to the BUDSIZE and fixed the documentation as well.
    
    Reported-by: syzbot+411debe54d318eaed386@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=411debe54d318eaed386
    Signed-off-by: Manas Ghandat <ghandatmanas@gmail.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/jfs/jfs_dmap.c
+++ fs/jfs/jfs_dmap.c
@@ -2840,7 +2840,9 @@ static int dbBackSplit(dmtree_t *tp, int leafno, bool is_ctl)
  *	leafno	- the number of the leaf to be updated.
  *	newval	- the new value for the leaf.
  *
- * RETURN VALUES: none
+ * RETURN VALUES:
+ *  0		- success
+ *	-EIO	- i/o error
  */
 static int dbJoin(dmtree_t *tp, int leafno, int newval, bool is_ctl)
 {
@@ -2867,6 +2869,10 @@ static int dbJoin(dmtree_t *tp, int leafno, int newval, bool is_ctl)
 		 * get the buddy size (number of words covered) of
 		 * the new value.
 		 */
+
+		if ((newval - tp->dmt_budmin) > BUDMIN)
+			return -EIO;
+
 		budsz = BUDSIZE(newval, tp->dmt_budmin);
 
 		/* try to join.
