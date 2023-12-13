8f0186b4cefb1a7d8677a9b39695e3a7baf9a0fe
Alexandre Ghiti <alexghiti@rivosinc.com>
2023-12-13 14:40:27 +0100

    riscv: Fix set_direct_map_default_noflush() to reset _PAGE_EXEC
    [ Upstream commit b8b2711336f03ece539de61479d6ffc44fb603d3 ]
    
    When resetting the linear mapping permissions, we must make sure that we
    clear the X bit so that do not end up with WX mappings (since we set
    PAGE_KERNEL).
    
    Fixes: 395a21ff859c ("riscv: add ARCH_HAS_SET_DIRECT_MAP support")
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Link: https://lore.kernel.org/r/20231213134027.155327-3-alexghiti@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/riscv/mm/pageattr.c
+++ arch/riscv/mm/pageattr.c
@@ -371,7 +371,7 @@ int set_direct_map_invalid_noflush(struct page *page)
 int set_direct_map_default_noflush(struct page *page)
 {
 	return __set_memory((unsigned long)page_address(page), 1,
-			    PAGE_KERNEL, __pgprot(0));
+			    PAGE_KERNEL, __pgprot(_PAGE_EXEC));
 }
 
 void __kernel_map_pages(struct page *page, int numpages, int enable)
