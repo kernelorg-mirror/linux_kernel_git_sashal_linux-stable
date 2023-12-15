041fc7c545c813ed50aac375041fc9094fc44e63
Peter Zijlstra <peterz@infradead.org>
2023-12-15 10:12:21 +0100

    cfi: Add CFI_NOSEAL()
    [ Upstream commit e9d13b9d2f99ccf7afeab490d97eaa5ac9846598 ]
    
    Add a CFI_NOSEAL() helper to mark functions that need to retain their
    CFI information, despite not otherwise leaking their address.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20231215092707.669401084@infradead.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/include/asm/cfi.h
+++ arch/x86/include/asm/cfi.h
@@ -9,6 +9,7 @@
  */
 
 #include <linux/cfi.h>
+#include <asm/ibt.h>
 
 #ifdef CONFIG_CFI_CLANG
 enum bug_trap_type handle_cfi_failure(struct pt_regs *regs);
@@ -18,5 +19,9 @@ static inline enum bug_trap_type handle_cfi_failure(struct pt_regs *regs)
 	return BUG_TRAP_TYPE_NONE;
 }
 #endif /* CONFIG_CFI_CLANG */
+
+#if HAS_KERNEL_IBT == 1
+#define CFI_NOSEAL(x)	asm(IBT_NOSEAL(__stringify(x)))
+#endif
 
 #endif /* _ASM_X86_CFI_H */
