edk2-porting/linux-next

bcachefs: Fix printing of device durability
BCH_MEMBER_DURABILITY() was not present initially; a value of 0 means
use the default, nonzero means use v - 1.

Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

fs/bcachefs/sb-members.c
@@ -266,7 +266,7 @@ static void member_to_text(struct printbuf *out,
 
 	prt_str(out, "Durability:");
 	prt_tab(out);
-	prt_printf(out, "%llu", BCH_MEMBER_DURABILITY(&m));
+	prt_printf(out, "%llu", BCH_MEMBER_DURABILITY(&m) ? BCH_MEMBER_DURABILITY(&m) - 1 : 1);
 	prt_newline(out);
 
 	prt_printf(out, "Discard:");

kernelorg-mirror/linux_kernel_git_sashal_linux-stable

[ Upstream commit f60250de329ae6dbf8aeb49ebb13bf0b79e86a1d ]
Signed-off-by: Sasha Levin <sashal@kernel.org>

fs/bcachefs/sb-members.c
@@ -261,7 +261,7 @@ static void member_to_text(struct printbuf *out,
 
 	prt_printf(out, "Discard:");
 	prt_tab(out);
-	prt_printf(out, "%llu", BCH_MEMBER_DISCARD(&m));
+	prt_printf(out, "%llu", BCH_MEMBER_DISCARD(&m) ? BCH_MEMBER_DURABILITY(&m) - 1 : 1);
 	prt_newline(out);
 
 	prt_printf(out, "Freespace initialized:");